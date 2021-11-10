cabd29cb38faf5b567eb67a1834b6f86275c28db
Hafizur <hafizurrahmanbu@gmail.com>
2021-11-10 20:40:12 +0600

    modified delete and update method

--- index.js
+++ index.js
@@ -24,8 +24,94 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 // console.log(uri);
 
 client.connect(err => {
-    const collection = client.db("test").collection("devices");
+    const productsCollection = client.db("luxurious-watch").collection("products");
+    const purchaseInformation = client.db("luxurious-watch").collection("purchase-info");
     // perform actions on the collection object
+
+    //get all products from db by get method
+    app.get('/products', async (req, res) => {
+        const products = await productsCollection.find({}).toArray();
+        res.send(products);
+    });
+
+    //find specific product by id
+    app.get('/purchase/:id', async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: ObjectId(id) };
+        const specificProduct = await productsCollection.findOne(filter);
+        res.send(specificProduct);
+    });
+
+    // store purchase info data to database
+    app.post('/purchase-info', async (req, res) => {
+        const query = req.body;
+        const purchaseInfo = await purchaseInformation.insertOne(query);
+        res.send(purchaseInfo);
+    });
+
+    // get purchase ifo from the database
+    app.get('/manage-all-orders', async (req, res) => {
+        const purchaseDetails = await purchaseInformation.find({}).toArray();
+        res.send(purchaseDetails);
+    });
+
+    //update data from database 
+    app.put('/shipped/:id', async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: ObjectId(id) };
+        const updateProduct = {
+            $set: { status: 'Shipped' }
+        };
+        const result = await purchaseInformation.updateOne(filter, updateProduct);
+        console.log(result);
+        res.send(result);
+    });
+
+    //delete order from database
+    app.delete('/delete-product/:id', async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: ObjectId(id) };
+        const deleteOrder = await purchaseInformation.deleteOne(filter);
+        res.send(deleteOrder);
+    });
+
+    app.delete('/manage-product/:id', async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: ObjectId(id) };
+        const deleteProduct = await productsCollection.deleteOne(filter);
+        res.send(deleteProduct);
+        console.log(deleteProduct);
+    })
+
+    //get user's all order
+    app.get('/my-order/:email', async (req, res) => {
+        const email = req.params.email;
+        const filter = { email: email };
+        const myOrders = await purchaseInformation.find(filter).toArray();
+        res.send(myOrders);
+    });
+
+    //cancel order
+    app.delete('/cancel-order/:id', async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: ObjectId(id) };
+        const result = await purchaseInformation.deleteOne(filter);
+        res.send(result);
+        console.log(result);
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
